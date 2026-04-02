3451a6e979669f066d19fedddf574e82e2578048
Hartono <149193643+Hartonoo@users.noreply.github.com>
2026-04-02 18:05:16 +0700

    ubah

--- src/data.js
+++ src/data.js
@@ -27,51 +27,24 @@ export const data = {
         { to: 'couple', text: 'Couple' },
         { to: 'events', text: 'Events' },
         { to: 'moment', text: 'Moment' },
-        { to: 'message', text: 'Message' }, // Tambahkan ini agar navigasi ke pesan ada
+        { to: 'message', text: 'Message' },
       ],
     },
     couple: {
       title: 'Meet Happy Couple',
-      groom: {
-        name: 'HARTONO',
-        photo: require('./assets/images/couple/1.jpg'),
-        status: 'GROOM',
-        socmed: { facebook: '', twitter: '', instagram: '#' },
-        description: 'The son of Mr. X and Mrs. Y',
-      },
-      bride: {
-        name: 'MILANI ROSMITA',
-        photo: require('./assets/images/couple/2.jpg'),
-        status: 'BRIDE',
-        socmed: { facebook: '', twitter: '', instagram: '#' },
-        description: 'The daughter of Mr. A and Mrs. B',
-      },
+      groom: { name: 'HARTONO', photo: require('./assets/images/couple/1.jpg'), status: 'GROOM', socmed: { facebook: '', twitter: '', instagram: '#' }, description: 'The son of Mr. X and Mrs. Y' },
+      bride: { name: 'MILANI ROSMITA', photo: require('./assets/images/couple/2.jpg'), status: 'BRIDE', socmed: { facebook: '', twitter: '', instagram: '#' }, description: 'The daughter of Mr. A and Mrs. B' },
     },
     events: {
-      ceremony: {
-        type: 'ceremony',
-        foto: require('./assets/images/events/ceremony.png'),
-        title: 'THE CEREMONY',
-        date: 'Saturday, September 18th 2027',
-        time: '09.00 AM - 11.00 AM',
-        address: { line1: 'Alamat Akad Nikah', line2: 'Kota Bandung', line3: '-' },
-      },
-      reception: {
-        type: 'reception',
-        foto: require('./assets/images/events/reception.png'),
-        title: 'THE RECEPTION',
-        date: 'Saturday, September 18th 2027',
-        time: '12.00 PM - 05.00 PM',
-        address: { line1: 'Gedung Resepsi', line2: 'Kota Bandung', line3: '-' },
-      },
+      ceremony: { type: 'ceremony', foto: require('./assets/images/events/ceremony.png'), title: 'THE CEREMONY', date: 'Saturday, September 18th 2027', time: '09.00 AM - 11.00 AM', address: { line1: 'Alamat Akad Nikah', line2: 'Kota Bandung', line3: '-' } },
+      reception: { type: 'reception', foto: require('./assets/images/events/reception.png'), title: 'THE RECEPTION', date: 'Saturday, September 18th 2027', time: '12.00 PM - 05.00 PM', address: { line1: 'Gedung Resepsi', line2: 'Kota Bandung', line3: '-' } },
     },
     moment: {
       images: [
         { src: require('./assets/images/moment/1.jpg'), thumbnail: require('./assets/images/moment/thumbnails/1.jpg'), thumbnailWidth: 320, thumbnailHeight: 200 },
-        { src: require('./assets/images/moment/2.jpg'), thumbnail: require('./assets/images/moment/thumbnails/2.jpg'), thumbnailWidth: 240, thumbnailHeight: 320 },
       ],
     },
-    // INI BAGIAN YANG TADI HILANG DAN BIKIN BLANK PUTIH
+    // INI YANG WAJIB ADA AGAR TIDAK ERROR MAP
     message: {
       title: 'Kirim Ucapan',
       description: 'Berikan doa restu untuk kami',
@@ -80,7 +53,7 @@ export const data = {
       ]
     },
     location: {
-      maps: 'https://www.google.com/maps',
+      maps: 'https://maps.google.com',
     },
   },
 };
